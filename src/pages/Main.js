ff4040a2fd13d79ad46f60e8ef17d71cfd314097
greencoffee1635 <166354@naver.com>
2021-11-18 05:38:46 +0900

    style: Main page 스타일 적용 및 Logo 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,39 +1,117 @@
 import React from "react";
 import styled from "styled-components";
 
+// components & elements
+import Layout from "../components/Layout";
 import Button from "../elements/Button";
+import theme from "../shared/theme";
 
 const Main = props => {
   const { history } = props;
   return (
-    <Container>
-      <Title>직업가치관검사</Title>
-      <InputWrap>
-        <Name>이름</Name>
-        <input placeholder="name" />
-        <Gender>성별</Gender>
-        <label>
-          <input type="radio" value="Male" name="gender" />
-          MALE
-        </label>
-        <label>
-          <input type="radio" value="Female" name="gender" />
-          FEMALE
-        </label>
-      </InputWrap>
-      <Button
-        _onClick={() => {
-          history.push("/start");
-        }}
-        text="검사시작"
-      />
-    </Container>
+    <Layout>
+      <Container>
+        <Logo>
+          <eliceLogo>{eliceLogo}</eliceLogo>
+        </Logo>
+        <Title>직업가치관검사</Title>
+        <Name>
+          <SubTitle>이름</SubTitle>
+          <NameInput placeholder="" />
+        </Name>
+        <SubTitle>성별</SubTitle>
+        <Gender>
+          <div>
+            <GenderInput type="radio" value="Male" name="gender" />
+            <label>MALE</label>
+          </div>
+          <div>
+            <GenderInput type="radio" value="Female" name="gender" />
+            <label>FEMALE</label>
+          </div>
+        </Gender>
+        <ButtonWrapper>
+          <Button
+            _onClick={() => {
+              history.push("/start");
+            }}
+            text="검사시작"
+          />
+        </ButtonWrapper>
+      </Container>
+    </Layout>
   );
 };
-const Container = styled.div``;
-const Title = styled.div``;
-const Name = styled.div``;
-const InputWrap = styled.div``;
-const Gender = styled.div``;
+
+const eliceLogo = "/* elice */";
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+const Logo = styled.span`
+  font-size: 2rem;
+  font-weight: 600;
+  color: ${props => theme.colors.main};
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  align-items: left;
+  margin-bottom: 1rem;
+`;
+
+const SubTitle = styled.p`
+  width: 10rem;
+  font-size: 1.4rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const Name = styled.div`
+  width: 10rem;
+  align-items: center;
+  padding-bottom: 1.5rem;
+`;
+
+const NameInput = styled.input`
+  width: 30rem;
+  height: 3.1rem;
+  font-size: 1.8rem;
+  /* 입력시 글자색 */
+  color: ${props => theme.colors.deepgray};
+  /* 배경색 */
+  background-color: ${props => theme.colors.lightgray};
+  /* 테두리색 */
+  border: ${props => theme.colors.gray} 1px solid;
+  border-radius: 6px;
+  :focus {
+    outline-color: ${props => theme.colors.deepgray};
+  }
+`;
+
+const Gender = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.2rem;
+  font-weight: 400;
+  padding-bottom: 1.5rem;
+`;
+
+const GenderInput = styled.input``;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+`;
 
 export default Main;
