f9708e65ca610d70a1053239f9f1827abcb09b52
greencoffee1635 <166354@naver.com>
2021-11-20 22:11:01 +0900

    feat: Test page 스타일 구현

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Test = () => {
+// components & elements
+import Layout from "../components/Layout";
+import Container from "../components/Container";
+import Button from "../elements/Button";
+import { head_1 } from "../elements/Text";
+import Logo from "../components/Logo";
+import ProgressBar from "../components/Progress";
+import Question from "../components/Question";
+
+const Test = props => {
+  const { history } = props;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Layout>
+      <Container>
+        <Logo />
+        <div>
+          <div>
+            <Title>검사 진행</Title>
+          </div>
+          <ProgressBar color={"#7979F7"} width={"30rem"} value={0} max={100} />
+        </div>
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <ButtonWrapper>
+          <Button
+            _onClick={() => {
+              history.push("/result");
+            }}
+            text="검사시작"
+          ></Button>
+        </ButtonWrapper>
+      </Container>
+    </Layout>
+  );
+};
 
-export default Test
+const Title = styled.h1`
+  ${head_1}
+  margin-top: 5rem;
+  align-items: left;
+  margin-bottom: 2rem;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+  margin-top: 3rem;
+`;
+
+export default Test;
