2005cea09cd4e6ea869c82ab9e41057d05a848b2
greencoffee1635 <166354@naver.com>
2021-11-23 22:23:05 +0900

    feat: Sample page - radio 미선택시 검사버튼 비활성화

--- src/pages/Sample.js
+++ src/pages/Sample.js
@@ -18,10 +18,12 @@ const Sample = props => {
   const [answer02, setAnswer02] = useState([]);
   const [error, setError] = useState("");
 
+  let valid = false;
+
   useEffect(() => {
     const fetch = async () => {
       try {
-        const question = (await fetchData())[0]
+        const question = (await fetchData())[0];
 
         setQuestion(question.question);
         setAnswer01(question.answer01);
@@ -47,22 +49,37 @@ const Sample = props => {
           <SubTitle>직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요. 가치의 뜻을 잘모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.</SubTitle>
         </div>
         <QuestionWrapper>
-          <QuestionExample key={question}>{question}</QuestionExample>
-          <AnswerExample>
+          <QuestionSample key={question}>{question}</QuestionSample>
+          <AnswerSample>
             <div>
-              <input type="radio" name="character" disabled={true} />
+              <input
+                type="radio"
+                name="character"
+                value="0"
+                onClick={() => {
+                  valid = true;
+                }}
+              />
               <label key={answer01}>{answer01}</label>
             </div>
             <div>
-              <input type="radio" name="character" disabled={true} />
+              <input
+                type="radio"
+                name="character"
+                value="1"
+                onClick={() => {
+                  valid = true;
+                }}
+              />
               <label key={answer02}>{answer02}</label>
             </div>
-          </AnswerExample>
+          </AnswerSample>
         </QuestionWrapper>
         <ButtonWrapper>
           <Button
             _onClick={() => {
-              history.push("/test");
+              if (valid) history.push("/test");
+              else setError("예시 항목을 선택해주세요.");
             }}
             text="검사시작"
           ></Button>
@@ -103,13 +120,13 @@ const QuestionWrapper = styled.div`
   margin: 1rem 0 0 0;
 `;
 
-const QuestionExample = styled.p`
+const QuestionSample = styled.p`
   ${sub_1}
   margin-bottom: 2rem;
   text-align: center;
 `;
 
-const AnswerExample = styled.div`
+const AnswerSample = styled.div`
   ${sub_2}
   width: 90%;
   display: flex;
