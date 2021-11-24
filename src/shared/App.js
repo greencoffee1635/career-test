6d4f8a2ca066b4e5732beacaf56851f2d5842309
greencoffee1635 <166354@naver.com>
2021-11-25 05:24:24 +0900

    feat: Result page 생성

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Sample from "../pages/Sample";
 import Test from "../pages/Test";
+import Result from "../pages/Result";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route exact path="/sample" component={Sample} />
         <Route exact path="/test" component={Test} />
+        <Route exact path="/result" component={Result} />
       </BrowserRouter>
     </div>
   );
