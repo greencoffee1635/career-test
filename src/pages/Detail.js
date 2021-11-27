84e5331810185886665b244809ffe53e67759457
greencoffee1635 <166354@naver.com>
2021-11-28 06:34:47 +0900

    style: 불필요한 주석 삭제

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -9,42 +9,35 @@ import { head_1, head_3 } from "../shared/textStyle";
 import Logo from "../components/Logo";
 import theme from "../shared/theme";
 
-
 const Result = props => {
   const { history } = props;
 
   return (
-      <Layout>
-        <Container>
-          <Logo />
-          <TitleWrapper>
-            <Title>직업가치관검사 결과표</Title>
-            <SubTitle>
-              검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게 생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해
-              생각해 볼 기회를 제공합니다.
-            </SubTitle>
-          </TitleWrapper>
-          <ButtonWrapper>
-            <Button
-              _onClick={() => {
-                history.push("/");
-              }}
-              text="다시검사하기"
-            />
-          </ButtonWrapper>
-        </Container>
-      </Layout>
+    <Layout>
+      <Container>
+        <Logo />
+        <Title>직업가치관검사 결과표</Title>
+        <SubTitle>
+          검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게 생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해
+          생각해 볼 기회를 제공합니다.
+        </SubTitle>
+        <ButtonWrapper>
+          <Button
+            _onClick={() => {
+              history.push("/");
+            }}
+            text="다시검사하기"
+          />
+        </ButtonWrapper>
+      </Container>
+    </Layout>
   );
 };
-
-const TitleWrapper = styled.article`
-  margin: 5rem 0 2rem 0;
-`;
 
 const Title = styled.h1`
   ${head_1}
   color: ${props => theme.colors.main};
-  margin-top: 0;
+  margin: 5rem 0 2rem 0;
 `;
 
 const SubTitle = styled.h2`
@@ -55,7 +48,7 @@ const ButtonWrapper = styled.article`
   width: 100%;
   display: flex;
   justify-content: right;
-  /* margin-top: 2rem; */
+  margin-top: 2rem;
 `;
 
 export default Result;
